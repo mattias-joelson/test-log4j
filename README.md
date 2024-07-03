6e23ed500876519a470434a709058cd95fbfc055
Mattias Joëlson <mattias@joelson.org>
2024-07-03 21:51:22 +0200

    Slf4jExample

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # test-log4j
 Testing out log4j.
+
+https://www.baeldung.com/slf4j-with-log4j2-logback
---
+++ src/main/java/org/joelson/test/log4j/Slf4jExample.java
@@ -0,0 +1,15 @@
+package org.joelson.test.log4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Slf4jExample {
+
+    private static Logger logger = LoggerFactory.getLogger(Slf4jExample.class);
+
+    public static void main(String[] args) {
+        logger.debug("Debug log message");
+        logger.info("Info log message");
+        logger.error("Error log message");
+    }
+}
