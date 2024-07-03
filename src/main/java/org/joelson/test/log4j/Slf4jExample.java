566040c576a10c57b447c844e1b1218e17559f02
Mattias Joëlson <mattias@joelson.org>
2024-07-03 23:56:21 +0200

    Finally getting log4j2.properties to work.

--- src/main/java/org/joelson/test/log4j/Slf4jExample.java
+++ src/main/java/org/joelson/test/log4j/Slf4jExample.java
@@ -1,13 +1,15 @@
 package org.joelson.test.log4j;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Slf4jExample {
 
-    private static Logger logger = LoggerFactory.getLogger(Slf4jExample.class);
+    private static final Logger logger = LoggerFactory.getLogger(Slf4jExample.class);
 
     public static void main(String[] args) {
+        logger.trace("Trace log message");
         logger.debug("Debug log message");
         logger.info("Info log message");
         logger.error("Error log message");
